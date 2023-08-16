18cbe076612d11f666ee1a4f6f053cce8ebcba7b
targithu <githubtar@gmail.com>
2023-08-16 22:10:26 +0530

    misc:changed logo

--- src/data/data.js
+++ src/data/data.js
@@ -78,7 +78,7 @@ export const data=[
         id:3,
         name:"ClubApp",
         image:co,
-        github:"https://github.com/tarun-hegde/SocialMediaManagement_Clubs",
+        github:"https://github.com/tarun-hegde/Social-Media-Task-Organizer",
         text:"ClubApp is a Full-Stack Web Application where student can list down all their College Clubs  tasks/subtasks specifying deadlines.",
         stack: [
             {
